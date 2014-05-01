5935b8fa1da552365083e6e2a3e559d22efa7a52
clancyjane <clancy3@gmail.com>
2014-05-01 13:58:50 -0700

    changes

--- ca_ods_sp/prtl.prod_build_ooh_point_in_time_child.StoredProcedure.sql
+++ ca_ods_sp/prtl.prod_build_ooh_point_in_time_child.StoredProcedure.sql
@@ -14,7 +14,7 @@ begin
 
 
 
-		declare @last_month_end datetime
+		declare @last_qtr_end datetime
 		declare @last_year_end datetime
 		declare @int_startDate int;
 		declare @int_endDate int;
@@ -22,9 +22,9 @@ begin
 		-- initialize variables
 		set @startDate='2000-01-01'
 		select @cutoff_date=cutoff_date from dbo.ref_Last_DW_Transfer
-		set @endDate=(select dateadd(dd,-1,[quarter]) from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
-		set @last_month_end=(select dateadd(mm,-1,[quarter]) from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
-		set @last_year_end=(select dateadd(yy,-1,[year]) from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
+		set @endDate=(select dateadd(dd,-1,[MONTH]) from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
+		set @last_qtr_end=(select [quarter] from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
+		set @last_year_end=(select [year] from dbo.CALENDAR_DIM where calendar_date=@cutoff_date)	
 				--first pull all episodes into a temp table to clean up dirty data
 		set @int_startDate=(select convert(varchar(8),@startDate,112));
 		set @int_endDate=(select convert(varchar(8),@endDate,112));
