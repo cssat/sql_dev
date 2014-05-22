4845bc58a4fed83d77386fd3546dc449c56066fb
clancyjane <clancy3@gmail.com>
2014-05-22 13:20:36 -0700

    update
    add update build_date added to end

---
+++ ca_ods_sp/build_ooh_cache_tables.sql
@@ -0,0 +1,21 @@
+
+
+EXECUTE  [prtl].[prod_build_cache_poc1ab_aggr] '2014-05-09'
+   
+GO
+
+EXECUTE  [prtl].prod_build_cache_poc1ab_entries_aggr '2014-05-09'
+   
+GO
+EXECUTE  [prtl].prod_build_cache_poc1ab_exits_aggr '2014-05-09'
+   
+GO
+EXECUTE  [prtl].prod_build_cache_pbcw3_aggr '2014-05-09'
+   
+GO
+EXECUTE  [prtl].prod_build_cache_outcomes_aggr '2014-05-09'
+   
+GO
+EXECUTE  [prtl].prod_build_cache_pbcp5_aggr '2014-05-09'
+   
+GO
