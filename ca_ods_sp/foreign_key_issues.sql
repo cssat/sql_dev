618cfcc25199153046f92e6f0c96b727814d49c9
clancyjane <clancy3@gmail.com>
2014-07-08 10:32:08 -0700

    update

--- ca_ods_sp/foreign_key_issues.sql
+++ ca_ods_sp/foreign_key_issues.sql
@@ -11,4 +11,15 @@ select id_investigation_assessment_Fact from SUBSTANCE_ABUSE_WIZARD_FACT
 except
 select id_investigation_assessment_Fact from INVESTIGATION_ASSESSMENT_FACT
 
+select id_case_dim from CASE_PARTICIPANT_STATUS_DIM
+except
+select id_case_dim from case_dim
+
+select ID_PEOPLE_DIM from CASE_PARTICIPANT_STATUS_DIM
+except
+select ID_PEOPLE_DIM from PEOPLE_DIM
+
+select id_case_dim from case_participant_fact
+except
+select id_case_dim from case_dim
 
