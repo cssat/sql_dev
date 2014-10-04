0f52c17722761404e72530810bf71e437c5216ce
clancyjane <clancy3@gmail.com>
2014-10-04 07:43:06 -0700

    update
    fixed logic for "n-1" households

--- ca_ods_sp/prtl.prod_build_rate_referrals_order_specific.StoredProcedure.sql
+++ ca_ods_sp/prtl.prod_build_rate_referrals_order_specific.StoredProcedure.sql
@@ -7,39 +7,56 @@ as
 	select tce.* 
 			,DENSE_RANK() over (partition by coalesce(grp_id_case,tce.id_intake_fact) 
 										order by coalesce(grp_rfrd_date,tce.rfrd_date)
-														,coalesce(intake_grouper,tce.id_intake_fact )asc) cps_hh_nth_order
-			,lead(coalesce(grp_rfrd_date,tce.rfrd_date)) over (partition by coalesce(grp_id_case,tce.id_intake_fact) 
-							order by coalesce(grp_rfrd_date,tce.rfrd_date)) nxt_rfrd_date
+														,coalesce(intake_grouper,tce.id_intake_fact )asc) nth_order
+			,cast(null as datetime)  nxt_rfrd_date
 	into #referrals
 	from [prtl].[vw_referrals_grp] tce
 	where  tce.cd_access_type in (1,4) and fl_dlr=0
 	and hh_with_children_under_18=1 
 
+	-- get next cps referral
+		update ref
+		set nxt_rfrd_date=nxt.rfrd_date
+		from #referrals ref
+		join #referrals nxt on ref.id_case=nxt.id_case and nxt.nth_order=ref.nth_order+1
+
+		--select count(*) from #referrals where cd_final_decision=1 541793
+
+
 
 CREATE NONCLUSTERED INDEX idx_start_date_nxt_rfrd_date_id_case_nth_order_scrn
 ON #referrals (cohort_entry_date,[nxt_rfrd_date])
-INCLUDE ([id_case],[cps_hh_nth_order])
+INCLUDE ([id_case],[nth_order])
 
 
---- get nth order households
-	if OBJECT_ID('tempDB..#households') is not null drop table #households;
-	select [month],curr.cps_hh_nth_order,cnty.cd_cnty county_cd,count(distinct curr.id_case) cnt_households
-	into #households
-	from 	 #referrals   curr 
-		join (select distinct [month] from calendar_dim 
-				where calendar_date between '1998-01-01' and (select dateadd(m,-1,(select cutoff_date from ref_last_dw_transfer)))
-				) mnth
-				on mnth.[month]>=curr.cohort_entry_date and (mnth.[month]< nxt_rfrd_date or nxt_rfrd_date is null)
-		join prm_cnty cnty on cnty.match_code=curr.intake_county_cd
-    where exists(select * from  base.tbl_household_children chld 
+--- get nth order At Risk households
+	if OBJECT_ID('tempDB..#nthOrderAtRiskHH') is not null drop table #nthOrderAtRiskHH;
+	select [month],n.nth_order,cnty.cd_cnty county_cd,count(distinct curr.id_case) cnt_households
+	into #nthOrderAtRiskHH
+from 	 #referrals   curr 
+	join (select number nth_order from numbers) n on n.nth_order in (1,2,3)
+				and curr.nth_order=n.nth_order-1
+	join (select distinct [month] from calendar_dim 
+			where calendar_date between '2000-01-01' and (select dateadd(m,-1,(select cutoff_date from ref_last_dw_transfer)))
+			) mnth
+			on mnth.[month]>=curr.cohort_entry_date 
+				and (mnth.[month]<= cast(convert(varchar(10),nxt_rfrd_date,121) as datetime)
+											or nxt_rfrd_date is null)
+	join prm_cnty cnty on cnty.match_code=curr.intake_county_cd
+    where exists(select intake_grouper from  base.tbl_household_children chld 
 								where  chld.id_intake_fact=curr.id_intake_fact 
 								and IIF(day([month]) < day(chld.dt_birth) and chld.[dt_birth]<[month]
 												, datediff(mm,chld.dt_birth,[month]) - 1
 												,datediff(mm,chld.dt_birth,[month])) < (18*12))
-	 group by [month],curr.cps_hh_nth_order,cnty.cd_cnty
-	order by  cnty.cd_cnty,[month],curr.cps_hh_nth_order
+		--exclude households that have an nth order prior to month
+		and not exists(select * from #referrals nRef
+				where nRef.id_case=curr.id_case
+				and nRef.cohort_entry_date<curr.cohort_entry_date
+				and nRef.nth_order>curr.nth_order)
+	 group by [month],n.nth_order,cnty.cd_cnty
+	order by  cnty.cd_cnty,[month],n.nth_order
 
-
+--	select * from #nthOrderAtRiskHH order by county_cd,month,nth_order
 	
 
 alter table prtl.rate_referrals_order_specific NOCHECK CONSTRAINT ALL
@@ -61,9 +78,9 @@ from  (select distinct [month] from calendar_dim
 				where calendar_date between '2004-01-01' and (select dateadd(m,-1,(select cutoff_date from ref_last_dw_transfer)))
 				) mnth
  join ref_lookup_county refC on refC.county_cd between 0 and 39
- join (select number nth_order from numbers ) n on n.nth_order between 1 and 2
+ join (select number nth_order from numbers ) n on n.nth_order between 1 and 3
  left join (
- 					select cps.cps_hh_nth_order  "nth_order"
+ 					select cps.nth_order  "nth_order"
 						,cohort_entry_date cohort_date
 						,cnty.cd_cnty
 						,date_type
@@ -71,7 +88,7 @@ from  (select distinct [month] from calendar_dim
 						,count(distinct cps.intake_grouper) cnt_referrals
 					from  #referrals cps 
 					join prm_cnty cnty on cps.intake_county_cd=cnty.match_code
-					group by cps.cps_hh_nth_order
+					group by cps.nth_order
 						,cohort_entry_date 
 						,cnty.cd_cnty
 						,date_type
@@ -79,8 +96,8 @@ from  (select distinct [month] from calendar_dim
 			) cps on cps.cohort_date=mnth.MONTH
 			and cps.nth_order=n.nth_order
 			and cps.cd_cnty=refC.county_cd
- left join #households h on h.MONTH=mnth.month 
-			and refC.county_cd=h.county_cd and  h.cps_hh_nth_order =n.nth_order-1
+ left join #nthOrderAtRiskHH h on h.MONTH=mnth.month 
+			and refC.county_cd=h.county_cd and  h.nth_order =n.nth_order
 left join (select measurement_year,county_cd,sum(pop_cnt) tot_pop
 							from public_data.census_population_household where cd_race<9
 							group by measurement_year,county_cd
@@ -93,7 +110,7 @@ left join (select measurement_year,county_cd,sum(pop_cnt) tot_pop
 order by refC.county_cd,mnth.[MONTH],n.nth_order
 		
 
-
+select * from prtl.rate_referrals_order_specific order by county_cd,start_date,nth_order
 
 
 
