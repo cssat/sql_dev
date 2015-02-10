b0c3709bd574dd15d9f255950d69f09f5a527218
oieeri26 <oieeri26@gmail.com>
2015-02-10 12:57:54 -0800

    updated mp sp's

--- Stored Procedures 1566 and 1774/sp_mp_rate_placement_ts.sql
+++ Stored Procedures 1566 and 1774/sp_mp_rate_placement_ts.sql
@@ -1,31 +1,3 @@
-DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_rate_placement_ts`(p_date varchar(3000), p_cd_region varchar(200),p_entry_point varchar(30))
-BEGIN
-SELECT 
-	CONVERT(TD.cohort_date, DATE) AS 'Cohort Date'
-	,TD.old_region_cd
-	,LC.old_region AS 'Region'
-	,TD.entry_point AS 'cd_access_type'
-	,A.tx_access_type AS 'Access type desc'
-	,ROUND(TD.rate_placement, 2) AS 'Placement Rate'
-	,ROUND(TD.trend, 2) AS 'Trend'
-	,ROUND(TD.seasonal, 2) AS 'Seasonal'
-FROM test_annie.rate_placement_ts AS TD
-	LEFT JOIN ref_filter_access_type AS A
-		ON TD.entry_point = A.cd_access_type
-	LEFT JOIN ref_lookup_region AS LC
-		ON TD.old_region_cd = LC.old_region_cd
-WHERE cohort_date >= '2009-07-01'
-	AND	find_in_set(TD.old_region_cd, p_cd_region) > 0
-	AND find_in_set(TD.entry_point, p_entry_point) > 0
-ORDER BY
-	TD.old_region_cd
-	,TD.cohort_date asc
-	,TD.entry_point;
-END$$
-DELIMITER ;
-
-DROP PROCEDURE `test_annie`.`sp_mp_rate_placement_ts`;
 
 
 DELIMITER $$
@@ -41,11 +13,6 @@ FROM test_annie.rate_placement_ts AS TD
 WHERE cohort_date >= '2009-07-01';
 END$$
 DELIMITER ;
-
-CALL `test_annie`.`sp_mp_rate_placement_ts`('0');
-
-
-
 
 
 
--- Stored Procedures 1566 and 1774/sp_mp_rate_referrals_order_ts.sql
+++ Stored Procedures 1566 and 1774/sp_mp_rate_referrals_order_ts.sql
@@ -1,32 +1,3 @@
-DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_rate_referrals_order_ts`(p_date varchar(3000),p_cd_region varchar(100),p_nth_order varchar(100))
-BEGIN
-
-SELECT DISTINCT
-	CONVERT(RR.start_date, DATE) AS 'Date'
-	,RR.old_region_cd
-	,LC.old_region AS 'Region'
-	,RR.nth_order AS 'cd_order'
-	,O.tx_order AS 'Order'
-	,ROUND(RR.referral_rate, 2) AS 'Placement Rate'
-	,ROUND(RR.trend, 2) AS 'Trend'
-	,ROUND(RR.seasonal, 2) AS 'Seasonal'
-FROM rate_referrals_order_specific_ts AS RR
-	JOIN ref_filter_order AS O
-		ON O.cd_order = RR.nth_order
-	JOIN ref_lookup_county_region AS LC
-		ON RR.old_region_cd = LC.old_region_cd
-WHERE RR.start_date >= '2009-07-01'
-	AND	find_in_set(RR.old_region_cd, p_cd_region) > 0
-	AND find_in_set(RR.nth_order, p_nth_order) > 0
-ORDER BY
-	RR.old_region_cd
-	,RR.start_date asc
-	,RR.nth_order;
-END$$
-DELIMITER ;
-
-DROP PROCEDURE `test_annie`.`sp_mp_rate_referrals_order_ts`;
 
 DELIMITER $$
 CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_referrals_order_ts`(p_date varchar(3000))
@@ -35,8 +6,8 @@ SELECT
 	CONVERT(start_date, DATE) AS 'Date'
 	,old_region_cd AS 'Region'
 	,nth_order - 1 AS 'Order'
-	,ROUND(referral_rate, 2) AS 'Placement Rate'
-	,ROUND(trend, 2) AS 'Trend'
+	,ROUND(referral_rate, 2) AS 'Scatterplot Values'
+	,ROUND(trend, 2) AS 'Trend Line Values'
 FROM rate_referrals_order_specific_ts
 WHERE start_date >= '2009-07-01'
 ORDER BY
@@ -45,3 +16,5 @@ ORDER BY
 	,nth_order;
 END$$
 DELIMITER ;
+
+
