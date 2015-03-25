ff7ea3d980caeda418af27af623adbb4343106ea
Ryan Schmitz <jacquesdereims@gmail.com>
2015-03-25 15:45:51 -0700

    fixed and cleaned up prm_ooh_census_population
    Updated the first query to not override all age grouping codes. Because
    the source table doesn't contain the zero value for any of the columns
    where it is being substituted, changed unions to union alls to improve
    performance. Cleaned up column labeling for readability.

--- ca_ods_views/dbo.prm_ooh_census_population.View.sql
+++ ca_ods_views/dbo.prm_ooh_census_population.View.sql
@@ -1,7 +1,7 @@
 USE [CA_ODS]
 GO
 
-/****** Object:  View [dbo].[prm_ooh_census_population]    Script Date: 6/25/2014 8:09:10 AM ******/
+/****** Object:  View [dbo].[prm_ooh_census_population]    Script Date: 3/25/2015 3:37:15 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,86 +10,88 @@ GO
 
 
 
+
 	 ALTER view  [dbo].[prm_ooh_census_population]
 				 as
 				 
-				 select measurement_year [measurement_year],0 age_grouping_cd,cd_race,pk_gndr,county_cd as county_cd,(pop_cnt)
-				 from  CA_ODS.dbo.ref_lookup_census_population  
-				union
-				--	 age
-				select measurement_year [start_year],0 age_grouping_cd,cd_race,pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				select measurement_year,age_grouping_cd,cd_race,pk_gndr,county_cd,pop_cnt
+				from dbo.ref_lookup_census_population
+				union all
+				-- age
+				select measurement_year,0 [age_grouping_cd],cd_race,pk_gndr,county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,cd_race,pk_gndr,county_cd
-				union
+				union all
 				-- race
-				select measurement_year [start_year],age_grouping_cd,0 cd_race,pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				select measurement_year,age_grouping_cd,0 [cd_race],pk_gndr,county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,pk_gndr,county_cd
-				union
+				union all
 				-- gender
-				select measurement_year [start_year],age_grouping_cd,cd_race,0 pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				select measurement_year,age_grouping_cd,cd_race,0 [pk_gndr],county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,cd_race,county_cd
-				union
+				union all
 				-- county
-				select measurement_year [start_year],age_grouping_cd,cd_race,pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				select measurement_year,age_grouping_cd,cd_race,pk_gndr,0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,pk_gndr,cd_race
-				-- age  race  race age
-				union 
-				select measurement_year [start_year],0 age_grouping_cd,0 cd_race,pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				-- age, race
+				union all
+				select measurement_year,0 [age_grouping_cd],0 [cd_race],pk_gndr,county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,pk_gndr,county_cd
-				union 
-				--age ,gender
-				select measurement_year [start_year],0 age_grouping_cd,cd_race,0 pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- age, gender
+				select measurement_year,0 [age_grouping_cd],cd_race,0 [pk_gndr],county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,cd_race,county_cd
-				union 
-				--age,county
-				select measurement_year [start_year],0 age_grouping_cd,cd_race,pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- age, county
+				select measurement_year,0 [age_grouping_cd],cd_race,pk_gndr,0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,cd_race,pk_gndr
-				union 
-				-- race,gender
-				select measurement_year [start_year], age_grouping_cd,0 cd_race,0 pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- race, gender
+				select measurement_year,age_grouping_cd,0 [cd_race],0 [pk_gndr],county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,county_cd
-				union 
-				--race,county
-				select measurement_year [start_year], age_grouping_cd,0 cd_race,pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- race, county
+				select measurement_year,age_grouping_cd,0 [cd_race],pk_gndr,0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,pk_gndr
 				-- gender, county
-				union
-				select measurement_year [start_year],age_grouping_cd,cd_race,0 pk_gndr,0  as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				select measurement_year,age_grouping_cd,cd_race,0 [pk_gndr],0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,age_grouping_cd,cd_race
-				union
-				--age, race,gender
-				select measurement_year [start_year],0 age_grouping_cd,0 cd_race,0 pk_gndr,county_cd as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- age, race, gender
+				select measurement_year,0 [age_grouping_cd],0 [cd_race],0 [pk_gndr],county_cd,sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,county_cd
-				union 
-				--age,gender,county
-				select measurement_year [start_year],0 age_grouping_cd,cd_race,0 pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
-				group by measurement_year,cd_race
-				union 
-				--race,gender,county
-				select measurement_year [start_year], age_grouping_cd,0 cd_race,0 pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
-				group by measurement_year,age_grouping_cd
-				union 
-				--	age,race,county
-				select measurement_year [start_year],0 age_grouping_cd,0 cd_race,pk_gndr,0 as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- age, race, county
+				select measurement_year,0 [age_grouping_cd],0 [cd_race],pk_gndr,0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year,pk_gndr
-				union
-				--age,race,gender,county
-				select measurement_year [start_year],0 age_grouping_cd,0 cd_race,0 pk_gndr,0  as county_cd,sum(pop_cnt)
-				from  CA_ODS.dbo.ref_lookup_census_population  
+				union all
+				-- age, gender, county
+				select measurement_year,0 [age_grouping_cd],cd_race,0 [pk_gndr],0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
+				group by measurement_year,cd_race
+				union all
+				-- race, gender, county
+				select measurement_year,age_grouping_cd,0 [cd_race],0 [pk_gndr],0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
+				group by measurement_year,age_grouping_cd
+				union all
+				-- age, race, gender, county
+				select measurement_year,0 [age_grouping_cd],0 [cd_race],0 [pk_gndr],0 [county_cd],sum(pop_cnt) [pop_cnt]
+				from  dbo.ref_lookup_census_population  
 				group by measurement_year
+
 
 
 GO
