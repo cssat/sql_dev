a30bbc0d0c685dbcd521fbbf8938a5468faa3afa
MENewby <newby@uw.edu>
2015-03-26 15:31:14 -0700

    reports order

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referrals_order_ts.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referrals_order_ts.sql
@@ -1,13 +1,13 @@
 
 DELIMITER $$
-CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_rate_referrals_order_ts`(p_date varchar(3000))
+CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_referrals_order_ts`(p_date varchar(3000))
 BEGIN
 SELECT 
 	CONVERT(start_date, DATE) AS 'Month/Year of Ordered Report'
 	,old_region_cd AS 'Region'
 	,nth_order - 1 AS 'Order'
 	,ROUND(referral_rate, 2) AS 'Scatterplot (Actual Values)'
-	,ROUND(trend, 2) AS 'Trend Line (Seasonal Variation)'
+	,ROUND(trend, 2) AS 'Trend Line (Seasonally Adjusted)'
 FROM rate_referrals_order_specific_ts
 WHERE start_date >= '2009-07-01'
 ORDER BY
