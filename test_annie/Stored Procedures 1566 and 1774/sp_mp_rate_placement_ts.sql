0becc3fab2a2fc28e16b8777b8704fa3fe6d3831
MENewby <newby@uw.edu>
2015-03-26 15:03:28 -0700

    placements

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_placement_ts.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_placement_ts.sql
@@ -1,9 +1,11 @@
 
+DROP PROCEDURE `test_annie`.`sp_mp_rate_placement_ts`;
+
 DELIMITER $$
 CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_placement_ts`(p_date varchar(3000))
 BEGIN
 SELECT 
-	CONVERT(cohort_date, DATE) AS 'Cohort Date'
+	CONVERT(cohort_date, DATE) AS 'Month/Year of Placement'
 	,old_region_cd AS 'Region'
 	,entry_point AS 'Access Type'
 	,ROUND(rate_placement, 2) AS 'Scatterplot (Actual Values)'
