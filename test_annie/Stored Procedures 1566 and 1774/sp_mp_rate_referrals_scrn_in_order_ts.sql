2abd1365817b5398066ee100e739b5e33b3ad74b
oieeri26 <oieeri26@gmail.com>
2015-03-26 10:40:38 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referrals_scrn_in_order_ts.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referrals_scrn_in_order_ts.sql
@@ -1,14 +1,15 @@
 
+
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_referrals_scrn_in_order_ts`(p_date varchar(3000))
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_rate_referrals_scrn_in_order_ts`(p_date varchar(3000))
 BEGIN
 
 SELECT 
-	CONVERT(start_date, DATE) AS 'Date'
+	CONVERT(start_date, DATE) AS 'Month/Year of Ordered Screened-In Report'
 	,old_region_cd AS 'Region'
 	,nth_order - 1 AS 'Order'
-	,ROUND(referral_rate, 2) AS 'Scatterplot Values'
-	,ROUND(trend, 2) AS 'Trend Line Values'
+	,ROUND(referral_rate, 2) AS 'Scatterplot (Actual Values)'
+	,ROUND(trend, 2) AS 'Trend Line (Seasonal Variation)'
 FROM rate_referrals_scrn_in_order_specific_ts
 WHERE start_date >= '2009-07-01'
 ORDER BY
