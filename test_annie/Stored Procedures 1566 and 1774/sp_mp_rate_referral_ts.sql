708cb6dd7551961ec385367dc061a33ed8a79749
oieeri26 <oieeri26@gmail.com>
2015-03-26 11:02:01 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referral_ts.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_referral_ts.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_referral_ts`(p_date varchar(3000))
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_rate_referral_ts`(p_date varchar(3000))
 BEGIN
 SELECT 
-	CONVERT(start_date, DATE) AS 'Date' 
+	CONVERT(start_date, DATE) AS 'Month/Year of Report' 
 	,old_region_cd AS 'Region'
 	,entry_point AS 'Access Type'
-	,ROUND(referral_rate, 4) AS 'Scatterplot Values'
-	,ROUND(trend, 4) AS 'Trend Line Values'
+	,ROUND(referral_rate, 2) AS 'Scatterplot (Actual Values)'
+	,ROUND(trend, 2) AS 'Trend Line (Seasonal Variation)'
 FROM rate_referrals_ts
 WHERE start_date >= '2009-07-01'
 ORDER BY
