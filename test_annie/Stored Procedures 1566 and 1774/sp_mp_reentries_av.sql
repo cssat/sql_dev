37e9cd7eb59889197a5a7972787a879844f208a9
oieeri26 <oieeri26@gmail.com>
2015-04-03 11:46:00 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_reentries_av.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_reentries_av.sql
@@ -1,11 +1,13 @@
+DROP PROCEDURE `test_annie`.`sp_mp_reentries_av`;
 
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_reentries_av`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_reentries_av`()
 BEGIN
 SELECT
 	CONVERT(CONCAT(state_fiscal_yyyy, '-07-01'), DATETIME) AS 'Fiscal Year/Year Exited Out-of-Home Care'
 	,old_region_cd AS 'Region'
 	,ROUND(per_reent * 100, 2) 'Percent'
-FROM test_annie.episode_reentries_av;
+FROM test_annie.episode_reentries_av
+WHERE state_fiscal_yyyy < 2014;
 END$$
 DELIMITER ;
