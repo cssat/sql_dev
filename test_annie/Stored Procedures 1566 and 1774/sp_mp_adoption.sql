9bf54fe509c6d35edf9d0d4c1dc9145d31a9b5ed
oieeri26 <oieeri26@gmail.com>
2015-04-04 15:37:13 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_adoption.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_adoption.sql
@@ -1,3 +1,4 @@
+
 DROP PROCEDURE `test_annie`.`sp_mp_adoption`;
 
 DELIMITER $$
@@ -7,6 +8,6 @@ SELECT
 	CONVERT(CONCAT(state_fiscal_yyyy, '-07-01'), DATETIME)  AS 'Fiscal Year/Year Child Became Legally Free'
 	,old_region_cd AS 'Region'
 	,ROUND(perc_adopt * 100, 2) AS 'Percent'
-FROM test_annie.permanency_incidence_adoption;
+FROM test_annie.mp_permanency_incidence_adoption;
 END$$
 DELIMITER ;
