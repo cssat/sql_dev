70ef58e067a725a4e71ef535215cae5cbb352b63
oieeri26 <oieeri26@gmail.com>
2015-04-04 13:40:48 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_adoption.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_adoption.sql
@@ -1,12 +1,12 @@
+DROP PROCEDURE `test_annie`.`sp_mp_adoption`;
+
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_adoption`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_adoption`()
 BEGIN
 SELECT
-	CONVERT(CONCAT(P.state_fiscal_yyyy, '-07-01'), DATETIME)  AS 'Fiscal Year/Year Child Became Legally Free'
-	,P.old_region_cd AS 'Region'
-	,ROUND(P.perc_adopt * 100, 2) AS 'Percent'
-FROM test_annie.permanency_incidence_adoption AS P
-	JOIN ref_lookup_region AS LC
-		ON P.old_region_cd = LC.old_region_cd;
+	CONVERT(CONCAT(state_fiscal_yyyy, '-07-01'), DATETIME)  AS 'Fiscal Year/Year Child Became Legally Free'
+	,old_region_cd AS 'Region'
+	,ROUND(perc_adopt * 100, 2) AS 'Percent'
+FROM test_annie.permanency_incidence_adoption;
 END$$
 DELIMITER ;
