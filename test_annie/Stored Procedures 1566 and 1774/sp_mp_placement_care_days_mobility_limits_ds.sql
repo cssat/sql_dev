5be3c81ff93420b45d7a6704966748f524f5bbf5
oieeri26 <oieeri26@gmail.com>
2015-04-06 14:37:56 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_mobility_limits_ds.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_mobility_limits_ds.sql
@@ -1,6 +1,8 @@
 
+DROP PROCEDURE `test_annie`.`sp_mp_placement_care_days_mobility_limits_ds`
+
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_placement_care_days_mobility_limits_ds`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_placement_care_days_mobility_limits_ds`(p_date varchar(3000))
 BEGIN
 SELECT
 	CONVERT(CONCAT(fiscal_yr, '-07-01'), DATETIME)  AS 'Fiscal Year/Year in Out-of-Home Care'
--- test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_mobility_limits_tds.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_mobility_limits_tds.sql
@@ -1,6 +1,8 @@
 
+DROP PROCEDURE `test_annie`.`sp_mp_placement_care_days_mobility_limits_tds`
+
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_placement_care_days_mobility_limits_tds`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_placement_care_days_mobility_limits_tds`(p_date varchar(3000))
 BEGIN
 SELECT
 	CONVERT(CONCAT(MAL.fiscal_yr, '-07-01'), DATETIME)  AS 'Fiscal Year/Year in Out-of-Home Care'
@@ -9,9 +11,7 @@ SELECT
 	,cd_movement 
 	,ROUND(MAL.movement_rate, 2) AS 'Rate'
 FROM test_annie.placement_care_days_mobility_limits_tds AS MAL;
-	-- JOIN ref_lookup_region AS LC
-		-- ON MAL.old_region_cd = LC.old_region_cd
-	-- JOIN ref_lookup_years_in_care AS C
-		-- ON MAL.years_in_care = C.cd_years_in_care;
 END$$
 DELIMITER ;
+
+
