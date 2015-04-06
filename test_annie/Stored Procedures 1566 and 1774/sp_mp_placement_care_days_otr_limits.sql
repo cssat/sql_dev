fed11efc0330af354f9bd7ef4dd1fda0df9fd8b2
oieeri26 <oieeri26@gmail.com>
2015-04-06 14:39:09 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_otr_limits.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_placement_care_days_otr_limits.sql
@@ -1,7 +1,8 @@
 
+DROP PROCEDURE `test_annie`.`sp_mp_placement_care_days_otr_limits`;
 
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_placement_care_days_otr_limits`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_placement_care_days_otr_limits`(p_date varchar(3000))
 BEGIN
 SELECT	
 	CONVERT(CONCAT(fiscal_yr, '-07-01'), DATETIME)  AS 'Fiscal Year/Year in Out-of-Home Care'
