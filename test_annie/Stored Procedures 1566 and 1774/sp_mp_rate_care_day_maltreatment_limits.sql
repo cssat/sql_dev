0ebec99b5d2f167de4c783d209083925315eb0d8
oieeri26 <oieeri26@gmail.com>
2015-04-06 14:27:11 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_care_day_maltreatment_limits.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_rate_care_day_maltreatment_limits.sql
@@ -1,12 +1,13 @@
+DROP PROCEDURE `test_annie`.`sp_mp_rate_care_day_maltreatment_limits`;
 
 DELIMITER $$
-CREATE DEFINER=`test_annie`@`localhost` PROCEDURE `sp_mp_rate_care_day_maltreatment_limits`()
+CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_rate_care_day_maltreatment_limits`(p_date varchar(3000))
 BEGIN
 SELECT
 	CONVERT(CONCAT(fiscal_yr, '-07-01'), DATETIME)  AS 'Fiscal Year'
 	,old_region_cd AS 'Region'
 	,ROUND(care_day_incident_rate, 2) AS 'Rate'
-	,(cnt_incidents * 1.0 /care_days * 1.0) * 100 AS 'Percent'
 FROM test_annie.rate_care_day_maltreatment_limits;
 END$$
 DELIMITER ;
+
