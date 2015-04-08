4688f56c2edfc3e625280555c16e24be1911dd2a
oieeri26 <oieeri26@gmail.com>
2015-04-08 09:59:53 -0700

    updated sp

--- test_annie/Stored Procedures 1566 and 1774/sp_mp_permanency_incidence_av.sql
+++ test_annie/Stored Procedures 1566 and 1774/sp_mp_permanency_incidence_av.sql
@@ -5,10 +5,10 @@ DELIMITER $$
 CREATE DEFINER=`test_annie` PROCEDURE `sp_mp_permanency_incidence_av`(p_date varchar(3000))
 BEGIN
 SELECT
-	CONVERT(CONCAT(P.state_fiscal_yyyy, '-07-01'), DATETIME)  AS 'Fiscal Year/Removal Year'
-	,P.old_region_cd AS 'Region'
-	,ROUND(P.perc_perm * 100, 2) AS 'Percent'
-FROM test_annie.permanency_incidence_av AS P;
+	CONVERT(CONCAT(year, '-07-01'), DATETIME)  AS 'Fiscal Year/Removal Year'
+	,old_region_cd AS 'Region'
+	,ROUND(perm * 100, 2) AS 'Percent'
+FROM test_annie.mp_permanency_incidence;
 
 END$$
 DELIMITER ;
