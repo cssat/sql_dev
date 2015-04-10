991f7c91bdd6946b950ea79a6b83c52c5f8e82c2
geohey <geohey@MXL2230QJY.clients.nebula2.washington.edu>
2015-04-10 14:11:15 -0700

    Latest SP from POC2. Add debugging.

--- ca_ods_sp/prtl.prod_build_rate_referrals_order_specific.StoredProcedure.sql
+++ ca_ods_sp/prtl.prod_build_rate_referrals_order_specific.StoredProcedure.sql
@@ -1,6 +1,13 @@
-----    exec prtl.prod_build_rate_referrals_order_specific
+﻿/****** Object:  StoredProcedure [prtl].[prod_build_rate_referrals_order_specific]    Script Date: 4/1/2015 11:00:21 AM ******/
+SET ANSI_NULLS ON
+GO
 
-alter procedure prtl.prod_build_rate_referrals_order_specific
+SET QUOTED_IDENTIFIER ON
+GO
+
+----    exec prtl.prod_build_rate_referrals_order_specific @debug = 0
+
+ALTER procedure [prtl].[prod_build_rate_referrals_order_specific] (@debug smallint = 0)
 as
 
 	if OBJECT_ID('tempDB..#referrals') is not null drop table #referrals;
@@ -20,11 +27,13 @@ as
 	set nxt_rfrd_date=nxt.rfrd_date
 	from #referrals ref
 	join #referrals nxt on ref.id_case=nxt.id_case and nxt.nth_order=ref.nth_order+1
-
-
+	
 	CREATE NONCLUSTERED INDEX idx_start_date_nxt_rfrd_date_id_case_nth_order_scrn
-	ON #referrals (cohort_entry_date,[nxt_rfrd_date])
+	ON #referrals ([cohort_entry_date] ASC,[nxt_rfrd_date] ASC)
 	INCLUDE ([id_case],[nth_order])
+
+	CREATE NONCLUSTERED INDEX idx_id_case
+	ON #referrals ([id_case] ASC, [cohort_entry_date] ASC, [nth_order] ASC)
 
 -- this is the denominator  which represents distinct households with exactly "n-1" prior referrals.
 --- get nth order At Risk households
@@ -35,12 +44,11 @@ from 	 #referrals   curr  -- all referrals
 	-- join nth order  on n.nth_order-1 since we are matching order 1 referral  with state households with children under 18
 	-- order 2 households are those receiving 2nd referral and they  belong to  households that have exactly 1 prior referral
 	-- order 3 households are those receiving 3nd referral and they  belong to  households that have exactly 2 prior referral
-	join (select number nth_order from numbers) n on n.nth_order in (1,2,3)
+	join (select number nth_order from numbers) n on n.nth_order <=100 --in (1,2,3,4,5,6,7,8,9,10) (Joe Changed this for AR)
 				and curr.nth_order=n.nth_order-1
 	join (select distinct [month] from calendar_dim 
 			where calendar_date between '2000-01-01' and (select dateadd(m,-1,(select cutoff_date from ref_last_dw_transfer)))
-			) mnth -- household is at risk for the nth order from the month they receive the nth order 
-			-- referral until the next referral date or the last referral date of record
+			) mnth
 			on mnth.[month]>=curr.cohort_entry_date 
 				and (mnth.[month]<= cast(convert(varchar(10),nxt_rfrd_date,121) as datetime)
 											or nxt_rfrd_date is null)
@@ -52,14 +60,23 @@ from 	 #referrals   curr  -- all referrals
 								and IIF(day([month]) < day(chld.dt_birth) and chld.[dt_birth]<[month]
 												, datediff(mm,chld.dt_birth,[month]) - 1
 												,datediff(mm,chld.dt_birth,[month])) < (18*12))
+		--exclude households that have an nth order prior to month
+		and not exists(select * from #referrals nRef
+				where nRef.id_case=curr.id_case
+				and nRef.cohort_entry_date<curr.cohort_entry_date
+				and nRef.nth_order>curr.nth_order)
 	 group by [month],n.nth_order,cnty.cd_cnty
 	order by  cnty.cd_cnty,[month],n.nth_order
 
---	 select * from #nthOrderAtRiskHH order by county_cd,month,nth_order
+--	select * from #nthOrderAtRiskHH order by county_cd,month,nth_order
 	
-
+	CREATE NONCLUSTERED INDEX idx_MONTH
+	ON #nthOrderAtRiskHH ([MONTH] ASC, [county_cd] ASC, [nth_order] ASC)
+		
 alter table prtl.rate_referrals_order_specific NOCHECK CONSTRAINT ALL
+
 truncate table prtl.rate_referrals_order_specific
+
 insert into prtl.rate_referrals_order_specific(nth_order,[start_date],date_type,qry_type,county_cd,cnt_referrals,prior_order_cnt_households,referral_rate)
 select distinct
 			 n.nth_order "nth_order"
@@ -77,7 +94,7 @@ from  (select distinct [month] from calendar_dim
 				where calendar_date between '2004-01-01' and (select dateadd(m,-1,(select cutoff_date from ref_last_dw_transfer)))
 				) mnth
  join ref_lookup_county refC on refC.county_cd between 0 and 39
- join (select number nth_order from numbers ) n on n.nth_order between 1 and 3
+ join (select number nth_order from numbers ) n on n.nth_order between 1 and 100
  left join (-- this  gives the count of nth order referrals
  					select ref.nth_order  "nth_order"
 						,cohort_entry_date cohort_date
@@ -105,8 +122,28 @@ left join (select measurement_year,county_cd,sum(pop_cnt) tot_pop
 							from public_data.census_population_household where cd_race<9
 							group by measurement_year
 						) pop on measurement_year=year(ref.cohort_date) and pop.county_cd=ref.cd_cnty
+--where year(mnth.MONTH)=2010 and refc.county_cd=0
 order by refC.county_cd,mnth.[MONTH],n.nth_order
 		
 
 --select * from prtl.rate_referrals_order_specific order by county_cd,start_date,nth_order
+
+
+	IF @debug = 1
+	begin
+		IF OBJECT_ID (N'debug.referrals_nthOrderAtRiskHH', N'U') IS NOT NULL
+			DROP TABLE debug.referrals_nthOrderAtRiskHH;
+
+		select * into debug.referrals_nthOrderAtRiskHH 
+		from #nthOrderAtRiskHH;
+
+		IF OBJECT_ID (N'debug.referrals_referrals', N'U') IS NOT NULL
+			DROP TABLE debug.referrals_referrals;
+
+		select * into debug.referrals_referrals 
+		from #referrals;
+	end
+
+GO
+
 
