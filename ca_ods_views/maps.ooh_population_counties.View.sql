691f50bc11efa77c7ffeef42c0ed2e1b14776b31
Ryan Schmitz <jacquesdereims@gmail.com>
2015-04-14 15:20:42 -0700

    removed ordering
    Apparently it is useless to attempt to order the output on a view in SQL
    Server.

--- ca_ods_views/maps.ooh_population_counties.View.sql
+++ ca_ods_views/maps.ooh_population_counties.View.sql
@@ -10,27 +10,23 @@ CREATE VIEW [maps].[ooh_population_counties]
 
 AS
 
-SELECT TOP 100 PERCENT *
-FROM (				 
-	SELECT
-	 measurement_year [date]
-	, age_grouping_cd [age_group]
-	, county_cd [geog]
-	, sum(pop_cnt) [datacol]
-	FROM public_data.census_population
-	GROUP BY measurement_year, age_grouping_cd, cd_race, county_cd
+SELECT
+ measurement_year [date]
+, age_grouping_cd [age_group]
+, county_cd [geog]
+, sum(pop_cnt) [datacol]
+FROM public_data.census_population
+GROUP BY measurement_year, age_grouping_cd, cd_race, county_cd
 
-	UNION ALL
-	-- all age groups
-	SELECT
-	  measurement_year [date]
-	, 0 [age_group]
-	, county_cd [geog]
-	, sum(pop_cnt) [datacol]
-	FROM public_data.census_population
-	GROUP BY measurement_year, cd_race, county_cd) X
-ORDER BY date desc, geog, age_group;
-
+UNION ALL
+-- all age groups
+SELECT
+  measurement_year [date]
+, 0 [age_group]
+, county_cd [geog]
+, sum(pop_cnt) [datacol]
+FROM public_data.census_population
+GROUP BY measurement_year, cd_race, county_cd
 
 
 
--- ca_ods_views/maps.ooh_population_regions.View.sql
+++ ca_ods_views/maps.ooh_population_regions.View.sql
@@ -10,31 +10,27 @@ CREATE VIEW [maps].[ooh_population_regions]
 
 AS
 
-SELECT TOP 100 PERCENT *
-FROM (				 
-	SELECT
-	  cp.measurement_year [date]
-	, cp.age_grouping_cd [age_group]
-	, cr.region_6_cd [geog]
-	, sum(cp.pop_cnt) [datacol]
-	FROM public_data.census_population cp
-		LEFT JOIN dbo.ref_lookup_county_region cr
-			ON cp.county_cd = cr.county_cd
-	GROUP BY cp.measurement_year, cp.age_grouping_cd, cr.region_6_cd
+SELECT
+  cp.measurement_year [date]
+, cp.age_grouping_cd [age_group]
+, cr.region_6_cd [geog]
+, sum(cp.pop_cnt) [datacol]
+FROM public_data.census_population cp
+	LEFT JOIN dbo.ref_lookup_county_region cr
+		ON cp.county_cd = cr.county_cd
+GROUP BY cp.measurement_year, cp.age_grouping_cd, cr.region_6_cd
 
-	UNION ALL
-	-- all ages
-	SELECT
-	  cp.measurement_year [date]
-	, 0 [age_group]
-	, cr.region_6_cd [geog]
-	, sum(cp.pop_cnt) [datacol]
-	FROM public_data.census_population cp
-		LEFT JOIN dbo.ref_lookup_county_region cr
-			ON cp.county_cd = cr.county_cd
-	GROUP BY cp.measurement_year, cr.region_6_cd
-) X
-ORDER BY date desc, geog, age_group;
+UNION ALL
+-- all ages
+SELECT
+  cp.measurement_year [date]
+, 0 [age_group]
+, cr.region_6_cd [geog]
+, sum(cp.pop_cnt) [datacol]
+FROM public_data.census_population cp
+	LEFT JOIN dbo.ref_lookup_county_region cr
+		ON cp.county_cd = cr.county_cd
+GROUP BY cp.measurement_year, cr.region_6_cd
 
 
 
