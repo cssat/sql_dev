ec7ab84643a34c9883689320bfdfe0cd2c9c4c99
Ryan Schmitz <schmitzr@uw.edu>
2015-10-09 16:26:49 -0700

    added prm_region_6 view to ca_ods to allow matching of regions to all in a join

---
+++ ca_ods/dbo/Views/prm_region_6.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[prm_region_6]
+AS
+SELECT DISTINCT region_6_cd
+	,region_6_cd [match]
+FROM dbo.ref_lookup_county_region
+WHERE region_6_cd != 0
+
+UNION ALL
+
+SELECT DISTINCT 0 [region_6_cd]
+	,region_6_cd [match]
+FROM dbo.ref_lookup_county_region
+WHERE region_6_cd != 0
