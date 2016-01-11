455c91b8ba1364c27e60eeca54b1597b09c2ea82
oieeri26 <oieeri26@gmail.com>
2016-01-11 10:53:29 -0800

    first new ar stored procedure

---
+++ test_annie/Stored Procedures 1566 and 1774/sp_ar_cfsr_permanency_re_entry.sql
@@ -0,0 +1,18 @@
+
+DELIMITER $$
+CREATE DEFINER=`test_annie`@`%` PROCEDURE `sp_ar_cfsr_permanency_re_entry`(p_date varchar(3000))
+BEGIN
+
+SELECT
+	CONVERT(CONCAT(dat_year, '-10-01'), DATETIME) AS 'Fiscal Year'
+	,region AS 'Region'
+	,sex AS 'Sex'
+	,race AS 'Race'
+	,age_cat AS 'Age Category'
+	,cd_discharge AS 'Discharge Type'
+	,re_entry AS 'Percent'
+FROM test_annie.annual_report_cfsr_permanency_re_entry
+WHERE cd_discharge != 2;
+
+END$$
+DELIMITER ;
