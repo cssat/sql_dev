4bae9c5441e509557bd4158d1b476508e9211885
Ryan Schmitz <schmitzr@uw.edu>
2016-01-20 15:39:15 -0800

    Add RODIS ICD9 and ICD10 tables and import processes

---
+++ ca_ods/rodis/Tables/ICD9_injury_codes.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [rodis].[ICD9_injury_codes] (
+	[ICD_9_code] VARCHAR(50)
+	,[EPH_intentional] SMALLINT
+	,[EPH_unintentional] SMALLINT
+	,[EPH_undetermined] SMALLINT
+	,[EPH_all_injury] SMALLINT
+	)
+GO
+
+CREATE NONCLUSTERED INDEX [idx_ICD9_injury_codes] ON [rodis].[ICD9_injury_codes] (
+	[ICD_9_code]
+	) INCLUDE (
+	[EPH_intentional]
+	,[EPH_unintentional]
+	,[EPH_undetermined]
+	,[EPH_all_injury]
+	)
+GO
