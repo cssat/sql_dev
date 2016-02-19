e2f1d913b48c4b3318554551a5c8e8237c10f8f0
oieeri26 <oieeri26@gmail.com>
2016-02-19 13:28:59 -0800

    Added the rest of the stored procedures

---
+++ ca_ods/annual_report/Stored Procedures/sp_ar_screened_in.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE annual_report.sp_ar_screened_in (@p_date VARCHAR(3000))
+AS
+SELECT DATEFROMPARTS(fiscal_year, 10, 1) AS 'Fiscal Year'
+	,region AS 'Region'
+	,sex AS 'Sex'
+	,race AS 'Race'
+	,age_cat AS 'Age Category'
+	,screened_in_rate AS 'Rate'
+FROM [annual_report].[non_cfsr_safety]
+ORDER BY fiscal_year
+	,region
+	,sex
+	,race
+	,age_cat;
