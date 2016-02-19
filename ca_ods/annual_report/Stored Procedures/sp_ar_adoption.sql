4b896c07c7d66ac27efe9aef3178792e0d66ef4a
oieeri26 <oieeri26@gmail.com>
2016-02-19 11:48:29 -0800

    adding stored procedures

---
+++ ca_ods/annual_report/Stored Procedures/sp_ar_adoption.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE annual_report.sp_ar_adoption (@p_date VARCHAR(3000))
+AS
+SELECT DATEFROMPARTS(dat_year, 10, 1) AS 'Fiscal Year'
+	,region AS 'Region'
+	,sex AS 'Sex'
+	,race AS 'Race'
+	,age_cat AS 'Age Category'
+	,ROUND(adopt_in_365 * 100, 2) AS 'Percent'
+FROM [annual_report].[non_cfsr_permanency]
+ORDER BY dat_year
+	,region
+	,sex
+	,race
+	,age_cat;
